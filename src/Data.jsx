68de32fcfe775767f1231bd0697f00013c72e5d6
Kavya Sree Nachu <kavyanachu24@gmail.com>
2024-09-13 12:57:55 +0530

    initial commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "kavya",
+        email: "kavya123@gmail.com",
+        id: 1
+    },
+    {
+        name: "sree",
+        email: "sree456@gmail.com",
+        id: 2
+    },
+    {
+        name: "chandu",
+        email: "chandhu789@gmail.com",
+        id: 3,
+    }
+]
